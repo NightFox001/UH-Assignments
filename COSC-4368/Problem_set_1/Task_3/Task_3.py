4e5de8334821320a9b191c9cd7c06917f0080998
NightFox001 <johnnyh961@gmail.com>
2021-02-23 19:22:10 -0600

    did some more big brain, done with prob a

--- COSC-4368/Problem_set_1/Task_3/Task_3.py
+++ COSC-4368/Problem_set_1/Task_3/Task_3.py
@@ -1,4 +1,6 @@
 import sys
+import csv
+
 from test.test_importlib.data03 import namespace
 
 global nva
@@ -19,118 +21,26 @@ def C3(B, C, D, E, F):
         return True
     return False
 
-
-
 def checkConstraintsA(B, C, D, E, F):
     if (C1(B, C, E, F) and C2(D, E, F) and C3(B, C, D, E, F)):
         return True
     return False
 
-def findAllSolutions():
+def printAll(B, C, D, E, F):
+    print('A =', (B + C + E + F), ', B =', B, ', C =', C, ', D =', D, ', E =', E, ', F =', F)
+
+def getSumBC(D, E, F):
+
+    return ((D**2-417)/E**2-(E+F))  
+
+def aV4():
+    # V3 nva = ...
+    # print('V4')
     global nva
+    maxE = 8
     numSolutions = 0
-    maxE = 50
+    firstNVA = 0
 
-    aMin = 50
-    bMin = 50
-    cMin = 50
-    dMin = 50
-    eMin = 50
-    fMin = 50
-    aMax = 0
-    bMax = 0
-    cMax = 0
-    dMax = 0
-    eMax = 0
-    fMax = 0
-
-    for b in range(1, 51):
-        nva += 1 #from assigning value to b
-        for c in range(1, 51):
-            nva += 1 #from assigning value to c
-            for e in range(1, maxE+1):
-                nva += 1 #from assigning value to e
-                for f in range(1, 51):
-                    nva += 1 #from assigning value to f
-                    d = e + f + 21
-                    nva += 1 #from assigning value to d
-                    a = (b + c + e + f)
-                    nva += 1 #from assigning value to a
-                    if (checkConstraintsA(b, c, d, e, f)):
-                        numSolutions += 1
-                        if (a < aMin): aMin = a
-                        if (a > aMax): aMax = a
-                        if (b < bMin): bMin = b
-                        if (b > bMax): bMax = b
-                        if (c < cMin): cMin = c
-                        if (c > cMax): cMax = c
-                        if (d < dMin): dMin = d
-                        if (d > dMax): dMax = d
-                        if (e < eMin): eMin = e
-                        if (e > eMax): eMax = e
-                        if (f < fMin): fMin = f
-                        if (f > fMax): fMax = f
-
-
-    print('Solutions found:', numSolutions)
-    print(aMin, '< a <', aMax)
-    print(bMin, '< b <', bMax)
-    print(cMin, '< c <', cMax)
-    print(dMin, '< d <', dMax)
-    print(eMin, '< e <', eMax)
-    print(fMin, '< f <', fMax)
-    
-
-def aV1():
-    # V1 73,000
-    minA = 4
-    maxA = 50 # 
-    maxE = 8
-    for f in range(1, 29):
-        nva += 1 #from assigning value to f
-        for e in range(1, maxE+1):
-            nva += 1 #from assigning value to e
-            d = e + f + 21
-            nva += 1 #from assigning value to d
-            for b in range(1, 50-(e+f)):
-                nva += 1 #from assigning value to b
-                for c in range(1, 50-(e+f)-b):
-                    nva += 1 #from assigning value to c
-                    a = (b + c + e + f)
-                    nva += 1 #from assigning value to a
-                    # print(nva)
-                    printAll(a, b, c, d, e, f)
-                    if (checkConstraintsA(a, b, c, d, e, f)):
-                        print('Found solution!')
-                        return
-
-def aV2():
-    # V2 nva = 452
-    # make b and c min possible values
-    global nva
-    maxE = 8
-    b = 1
-    nva += 1 #from assigning value to b
-    c = 1
-    nva += 1 #from assigning value to c
-    for f in range(1, 29):
-        nva += 1 #from assigning value to f
-        for e in range(1, maxE+1):
-            nva += 1 #from assigning value to e
-            d = e + f + 21
-            nva += 1 #from assigning value to d
-            a = (b + c + e + f)
-            nva += 1 #from assigning value to f
-            # print(nva)
-            printAll(b, c, d, e, f)
-            if (checkConstraintsA(b, c, d, e, f)):
-                return
-
-def aV3():
-    # V3 nva = 452
-    print('V3')
-    global nva
-    maxE = 8
     aMin, aMax = 50, 0
     bMin, bMax = 50, 0
     cMin, cMax = 50, 0
@@ -138,60 +48,85 @@ def aV3():
     eMin, eMax = 50, 0
     fMin, fMax = 50, 0
     numSolutions = 0
-    firstNVA = 0
 
-    for e in range(maxE, 0, -1):
+    for e in range(maxE, 0, -1):        # rule H9
         nva += 1 #from assigning value to e
-        for f in range(1, 30-e):
+        for f in range(29-e, 0, -1):        # rule H12
             nva += 1 #from assigning value to f
-            d = e + f + 21
+
+            d = e + f + 21              # rule H2
             nva += 1 #from assigning value to d
-            for b in range(1, 50):
+
+
+            sumBC = getSumBC(d, e, f)   #H11
+            if (not sumBC%1==0 or sumBC>(50-e-f)): continue
+            nva += 1 #from assigning value to sumBC
+            # print('e =', e, 'f =', f, 'sum =', sumBC)
+            for b in range(1, 50-e-f): #
                 nva += 1 #from assigning value to b
-                for c in range(1, 50):
+                for c in range(1, 50-e-f-b):
                     nva += 1 #from assigning value to c
-                    a = (b + c + e + f)
-                    nva += 1 #from assigning value to f
-                    # printAll(b, c, d, e, f)
 
                     if (checkConstraintsA(b, c, d, e, f)):
-                        printAll(b, c, d, e, f)
-                        if numSolutions==0: firstNVA = nva
+                        if (numSolutions==0): 
+                            firstNVA = nva
+                            # printAll(b, c, d, e, f)
                         numSolutions += 1
-                        if (a < aMin): aMin = a
-                        if (a > aMax): aMax = a
-                        if (b < bMin): bMin = b
-                        if (b > bMax): bMax = b
-                        if (c < cMin): cMin = c
-                        if (c > cMax): cMax = c
-                        if (d < dMin): dMin = d
-                        if (d > dMax): dMax = d
-                        if (e < eMin): eMin = e
-                        if (e > eMax): eMax = e
-                        if (f < fMin): fMin = f
-                        if (f > fMax): fMax = f
 
-    print('Solutions found:', numSolutions)
     print('Fist solution found at nva:', firstNVA)
-    print(aMin, '< a <', aMax)
-    print(bMin, '< b <', bMax)
-    print(cMin, '< c <', cMax)
-    print(dMin, '< d <', dMax)
-    print(eMin, '< e <', eMax)
-    print(fMin, '< f <', fMax)
+    print('Solutions found:', numSolutions)
 
 
-def printAll(B, C, D, E, F):
-    print('A =', (B + C + E + F), ', B =', B, ', C =', C, ', D =', D, ', E =', E, ', F =', F)
-    
+def bV1():
+    global nva
+    maxE = 8
+    numSolutions = 0
+    firstNVA = 0
+
+    aMin, aMax = 50, 0
+    bMin, bMax = 50, 0
+    cMin, cMax = 50, 0
+    dMin, dMax = 50, 0
+    eMin, eMax = 50, 0
+    fMin, fMax = 50, 0
+    numSolutions = 0
+
+    for e in range(maxE, 0, -1):        # rule H9
+        nva += 1 #from assigning value to e
+        for f in range(29-e, 0, -1):        # rule H12
+            nva += 1 #from assigning value to f
+
+            d = e + f + 21              # rule H2
+            nva += 1 #from assigning value to d
+
+
+            sumBC = getSumBC(d, e, f)   #H11
+            if (not sumBC%1==0 or sumBC>100): continue
+            nva += 1 #from assigning value to sumBC
+            # print('e =', e, 'f =', f, 'sum =', sumBC)
+            for b in range(1, 50-e-f): #
+                nva += 1 #from assigning value to b
+                for c in range(1, 50-e-f-b):
+                    nva += 1 #from assigning value to c
+                    printAll(b, c, d, e, f)
+
+                    if (checkConstraintsA(b, c, d, e, f)):
+                        if (numSolutions==0): 
+                            firstNVA = nva
+                        numSolutions += 1
+
+    print('Fist solution found at nva:', firstNVA)
+    print('Solutions found:', numSolutions)
 
 def problemA():
-    print('Problem a')
-    global nva
-    maxB = 29
-    maxE = 8
-    aV3()
+    print('Starting Problem a...')
+    aV4()
     return
+
+
+def problemB():
+    print('Starting Problem b...')
+    bV1()
 
 def runTest():
     problemA()
@@ -205,7 +140,22 @@ else:
     validInput = False
     while(not validInput):
         usrInput = input("Enter problem to solve ('a', 'b', 'c'):\n")
-        if (usrInput == 'a' or usrInput == 'b' or usrInput == 'c'): validInput = True
+        if (usrInput == 'a'):
+            problemA()
+            validInput = True
+        if usrInput == 'b': 
+            validInput = True
+        if usrInput == 'c': 
+            validInput = True
+        if usrInput == 'all': 
+            findAllSolutions()
+            validInput = True
         else: print('\nInvalid input')
 
-print('nva =', nva)
+# with open('employee_file.csv', mode='w') as employee_file:
+#     employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+#     employee_writer.writerow(['John Smith', 'Accounting', 'November'])
+#     employee_writer.writerow(['Erica Meyers', 'IT', 'March'])
+
+print('Total possible combitnations =', nva)
